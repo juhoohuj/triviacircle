00c8148ce0d4dc2b89a1f3c3c1116eda59fafdcd
JUHO AHONEN <juho.ahonen2@gmail.com>
2024-02-06 20:21:12 +0200

    example

---
+++ content/questions.js
@@ -0,0 +1,22 @@
+
+
+
+const questions = [
+  {
+    question: "Is a programming language",
+    correct: [
+      "JavaScript",
+      "Python",
+      "Java",
+      "C++",
+      "Ruby"
+    ],
+    incorrect: [
+      "HTML",
+      "CSS",
+      "XML",
+      "JSON",
+      "SQL"
+    ]
+  }
+];
