c81a34f78330afe40803a28c05f72ab106e0dbe5
JUHO AHONEN <juho.ahonen2@gmail.com>
2024-02-15 23:55:52 +0200

    que

--- content/questions.js
+++ content/questions.js
@@ -18,6 +18,78 @@ const questions = [
       "JSON",
       "SQL"
     ]
+  },
+
+  {
+    question: "Is a front-end framework",
+    correct: [
+      "React",
+      "Angular",
+      "Vue",
+      "Ember",
+      "Backbone"
+    ],
+    incorrect: [
+      "Express",
+      "Meteor",
+      "Sails",
+      "Koa",
+      "Nest"
+    ]
+  },
+
+  {
+    question: "Is a back-end framework",
+    correct: [
+      "Express",
+      "Meteor",
+      "Sails",
+      "Koa",
+      "Nest"
+    ],
+    incorrect: [
+      "React",
+      "Angular",
+      "Vue",
+      "Ember",
+      "Backbone"
+    ]
+  },
+
+  {
+    question: "Is a database",
+    correct: [
+      "MySQL",
+      "PostgreSQL",
+      "SQLite",
+      "MongoDB",
+      "CouchDB"
+    ],
+    incorrect: [
+      "Express",
+      "Meteor",
+      "Sails",
+      "Koa",
+      "Nest"
+    ]
+  },
+
+  {
+    question: "Is a cloud provider",
+    correct: [
+      "AWS",
+      "Azure",
+      "Google Cloud",
+      "IBM Cloud",
+      "Oracle Cloud"
+    ],
+    incorrect: [
+      "Express",
+      "Meteor",
+      "Sails",
+      "Koa",
+      "Nest"
+    ]
   }
 ];
 
