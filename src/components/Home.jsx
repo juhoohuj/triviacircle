197f56613a8a4e8d204cf0c06cfaaef1b04fa9e2
Juho Ahonen <juho.ahonen2@gmail.com>
2024-02-22 19:12:02 +0200

    home

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>Enter your name: </p>
+            <input type="text" />
+
+            <button>Create Game</button>
+
+            <p>Join to an existing game: </p>
+            <input type="text" placeholder="Roomcode..." />
+
+        </div>
+    )
+}
+
+export default Home
+
