145d9ac9239ddab5e482b050bcb37b508c8bca2c
Juho Ahonen <juho.ahonen2@gmail.com>
2024-03-19 17:38:24 +0200

    createroom joinroom

--- src/services/rooms.js
+++ src/services/rooms.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
-
+const baseUrl = 'http://localhost:3000'; // Add http:// protocol specifier
 
 const joinRoom = async (roomId, username) => {
-  const response = await axios.post('/joinroom', {
-    roomId,
-    username,
-  });
-  return response.data;
+  try {
+    const response = await axios.post(`${baseUrl}/joinroom`, {
+      roomId,
+      username,
+    });
+    return response.data;
+  } catch (error) {
+    // Handle error
+    console.error('There was an error!', error);
+    throw error; // Rethrow error for caller to handle if necessary
+  }
+}
+
+const getOne = async id => {
+  const response = await axios.get(`${baseUrl}/${id}`)
+  return response.data
 }
 
 const createRoom = async (username) => {
-  const response = await axios.post('/createroom', {
+  const response = await axios.post(`${baseUrl}/createroom`, {
     username,
   });
   return response.data;
