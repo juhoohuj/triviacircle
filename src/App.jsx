e7ef3e48ee83130eed2ddb82ab6e749ffedccbe8
JUHO AHONEN <juho.ahonen2@gmail.com>
2024-05-18 23:48:24 +0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import Home from './components/Home';
 import RoomView from './components/RoomView';
+import './App.css';
 
 const App = () => {
   return (
-    <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/room/:roomId" element={<RoomView />} />
-        </Routes>
-      </Router>
-    </UserProvider>
+    <div className="App">
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/:roomId" element={<RoomView />} />
+          </Routes>
+        </Router>
+      </UserProvider>
+    </div>
   );
 };
 
